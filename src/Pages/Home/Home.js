5a550158ad3f86ed7bbd2207d5fcf8f62a07ab62
neha-aneja <neha.aneja1993@gmail.com>
2021-05-21 14:53:41 +0530

    Description added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="container">
-            <h2 className="text-center" style={{paddingTop:"30%"}}>
+            <h3 className="text-center" style={{paddingTop:"30%"}}>
                 Home
-            </h2>
+            </h3>
+            <h6 className="text-center">A page to be displayed first</h6>
         </div>
     )
 }
