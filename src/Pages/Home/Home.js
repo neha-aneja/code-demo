e8370c8797fcc3d1a25cc8e143305e075981c8aa
neha-aneja <neha.aneja1993@gmail.com>
2021-05-21 14:59:20 +0530

    heading fixed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="container">
-            <h3 className="text-center" style={{paddingTop:"30%"}}>
+            <h2 className="text-center" style={{paddingTop:"30%"}}>
                 Home
-            </h3>
+            </h2>
             <h6 className="text-center">A page to be displayed first</h6>
         </div>
     )
