f0c4467760e21492edf1c26f16d85c432809c013
neha-aneja <neha.aneja1993@gmail.com>
2021-05-21 14:30:50 +0530

    Home changed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,12 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="container">
-            <h2 className="text-center" style={{paddingTop:"30%"}}>
-                Home Page
-            </h2>
-            <h3 className="text-center">
-                A page to be displayed first
-            </h3>
+            <h1 className="text-center" style={{paddingTop:"30%"}}>
+                Home
+            </h1>
         </div>
     )
 }
