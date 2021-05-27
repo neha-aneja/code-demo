3d9d4da61e34029628ced31fd3149eabb41ced99
neha-aneja <neha.aneja1993@gmail.com>
2021-05-27 17:16:59 +0530

    use of cards and map

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import Card from './Card';
 
 const About = () => {
+
+    const arr = [
+        {
+            img: "Images/react.png",
+            title: "React",
+            desc: "React is a JavaScript library for building user interfaces. Learn what React is all about on our homepage.",
+            link: "https://reactjs.org/docs/getting-started.html"
+        },
+        {
+            img: "Images/bootstrap.png",
+            title: "Bootstrap",
+            desc: "Bootstrap, the world’s most popular framework for building responsive, mobile-first sites, with jsDelivr and a template starter page.",
+            link: "https://getbootstrap.com/docs/4.1/getting-started/introduction/"
+        },
+        {
+            img: "Images/redux.jpg",
+            title: "Redux",
+            desc: "Redux, A predictable state container for JS apps.",
+            link: "https://redux.js.org/"
+        }
+    ]
+
+
     return (
+
+        <>
+        <h1 style={{textAlign:"center"}}>Demo</h1> <br/>
+        
+
         <div className="container">
-            <h1 className="text-center" style={{paddingTop:"30%"}}>
-                About Us
-            </h1>
-        </div>
+        <div className="row justify-content-evenly">
+        {
+        arr.map( (val) => (
+        
+            <div className="col-lg-3 col-12">
+            <Card
+                img={val.img}
+                title={val.title}
+                desc={val.desc}
+                link={val.link}
+            />
+            </div>
+        ) )
+    }
+        </div></div>
+        
+        </>
     )
 }
 export default About;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,11 +2,47 @@ import React from 'react';
 
 const Home = () => {
     return (
+
         <div className="container">
-            <h2 className="text-center" style={{paddingTop:"30%"}}>
-                Home
-            </h2>
-            <h6 className="text-center">A page to be displayed first</h6>
+            <div className="row">
+                <div className="col-sm-4 mx-auto pt-3">
+                <label className="grid">Email</label><br/>
+                <input type="text" className="grid"/>
+                </div>
+                <div className="col-sm-4 mx-auto">
+                <label className="grid">Password</label><br/>
+                <input type="text" className="grid"/>
+                </div>
+                <div className="col-sm-4 mx-auto">
+                <button className="button">Submit</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-true mx-auto pt-3">
+                <label className="grid">Email</label><br/>
+                <input type="text" className="grid"/>
+                </div>
+                <div className="col-sm-true mx-auto">
+                <label className="grid">Password</label><br/>
+                <input type="text" className="grid"/>
+                </div>
+                <div className="col-sm-true mx-auto ">
+                <button className="button">Submit</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-true mx-auto pt-3">
+                <label className="grid">Email</label><br/>
+                <input type="text" className="grid"/>
+                </div>
+                <div className="col-sm-true mx-auto">
+                <label className="grid">Password</label><br/>
+                <input type="text" className="grid"/>
+                </div>
+                <div className="col-sm-true mx-auto ">
+                <button className="button">Submit</button>
+                </div>
+            </div>
         </div>
     )
 }
